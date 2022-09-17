d057cfaf8879825ca628b28bff145684ab5aae7e
kleber.barilli <kleber.barilli73@gmail.com>
2022-09-17 12:52:40 -0300

    setup ejs files

--- api/src/shared/server.ts
+++ api/src/shared/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import fetch from 'node-fetch';
+import path from 'path';
 import 'express-async-errors';
 import cors from 'cors';
 
@@ -10,9 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.set('view engine', 'ejs');
-app.use(express.static('../../../web/public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req: Request, res: Response) => {
+	res.render('index');
+});
 app.post('/', (req: Request, res: Response) => {});
 
 app.listen(process.env.PORT, () => {
