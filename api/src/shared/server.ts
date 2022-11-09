c7e2a198538c9db1eb08c6a7cedb10ca2e4a4557
kleber.barilli <kleber.barilli73@gmail.com>
2022-11-08 21:32:47 -0300

    wip

--- api/src/shared/server.ts
+++ api/src/shared/server.ts
@@ -17,7 +17,33 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req: Request, res: Response) => {
 	res.render('index');
 });
-app.post('/', (req: Request, res: Response) => {});
+app.post('/convert-mp3', async (req: Request, res: Response) => {
+	const videoUrl = req.body.videoUrl;
+	if (!videoUrl) {
+		return res.render('index', {
+			success: false,
+			message: 'Please, enter a video URL',
+		});
+	}
+	const api = await fetch(
+		`https://youtube-mp36.p.rapidapi.com/dl?id=${videoUrl}`,
+		{
+			method: 'GET',
+			headers: {
+				'x-rapidapi-key': process.env.RAPID_API_KEY || '',
+				'x-rapidapi-host': process.env.RAPID_API_HOST || '',
+			},
+		},
+	);
+	const response = await api.json();
+	if (response == 'ok') {
+		return res.render('index', {
+			success: true,
+			song_title: response.title,
+			song_link: response.link,
+		});
+	}
+});
 
 app.listen(process.env.PORT, () => {
 	console.log(`Rodando na porta ${process.env.PORT}`);
